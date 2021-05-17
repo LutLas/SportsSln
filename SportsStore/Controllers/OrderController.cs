4dc9f60d48f51e1f94749cc72267fd90f8416934
Lutaka Chihota <lutakachihota@gmail.com>
2021-05-17 11:17:38 +0200

    we on

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -9,11 +9,33 @@ namespace SportsStore.Controllers
 {
     public class OrderController : Controller
     {
-        public ViewResult Checkout() => View(new Order());
-        /*public IActionResult Index()
-        {
+        private IOrderRepository repository;
+        private Cart cart;
 
-            return View();
-        }*/
+        public OrderController(IOrderRepository repoService, Cart cartService)
+        {
+            repository = repoService;
+            cart = cartService;
+        }
+        public ViewResult Checkout() => View(new Order());
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                repository.SaveOrder(order);
+                cart.Clear();
+                return RedirectToPage("/Completed", new { orderId = order.OrderID });
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
