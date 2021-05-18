a7be30fa6d554c4c8b7e639adff50865a81cb3c9
Lutaka Chihota <lutakachihota@gmail.com>
2021-05-18 22:28:14 +0200

    give em

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -36,6 +36,7 @@ namespace SportsStore
             services.AddSession();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +66,8 @@ namespace SportsStore
                     new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
             SeedData.EnsurePopulated(app);
         }
